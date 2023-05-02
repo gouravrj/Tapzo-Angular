0f7866cdb669ad17dd7491f679470099b7ed1e37
Gourab Raj <gourav.raj42001@gmail.com>
2023-05-02 15:40:21 +0530

    1-Lender Registration/Login (Bike ADD_EDIT_DEL)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './lender/login/login.component';
+import { RegisterComponent } from './lender/register/register.component';
+import { ListComponent } from './bike/list/list.component';
+import { EditComponent } from './bike/edit/edit.component';
+import { AddComponent } from './bike/add/add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomePageComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"bike/list",component:ListComponent},
+  {path:"bike/edit/:id",component:EditComponent},
+  {path:"save",component:AddComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './lender/login/login.component';
+import { RegisterComponent } from './lender/register/register.component';
+import { AddComponent } from './bike/add/add.component';
+import { ListComponent } from './bike/list/list.component';
+import { EditComponent } from './bike/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
